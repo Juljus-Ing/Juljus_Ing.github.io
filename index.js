ece01cce368e1b1822fe89498e6247e472578651
Julian Naranjo <junaranjo6@poligran.edu.co>
2024-10-04 13:45:16 -0500

    Contact Page Functionality

---
+++ index.js
@@ -0,0 +1,29 @@
+emailjs.init('mY1lMVNmLYpx_B7fF');
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    //Código para obtener los valores del form
+
+    const name = document.getElementById('user-name').value;
+    const email = document.getElementById('user-email').value;
+    const message = document.getElementById('message').value;
+
+    emailjs.send('service_lmfi90w', 'template_143ao5j',{
+        to_name: "Recipient Name",
+        user_name: name,
+        user_email: email,
+        message: message,
+    })
+    .then(function(response){
+        iziToast.show({
+            message: 'Tu mensaje fue enviado correctamente!',
+            position: 'topLeft',
+            color: 'green',
+        })
+    }), function(error) {
+        iziToast.show({
+            message: 'Hubo un error al enviar el mensaje',
+            position: 'topLeft',
+            color: 'red',
+        })
+    }
+})
